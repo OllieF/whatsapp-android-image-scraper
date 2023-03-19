e85ab0339876a2935c31f6a4c62fe2578eb56676
Oliver Fritz <oliverfritz95@gmail.com>
2023-03-19 14:00:31 +0000

    Updated Readme and video

--- README.md
+++ README.md
@@ -1,32 +1,80 @@
-# Resources
-The following resources have been used:
-- [pure-python-adb](https://pypi.org/project/pure-python-adb/)
-- [abd](https://www.xda-developers.com/install-adb-windows-macos-linux/)
-- [scrcpy](https://github.com/Genymobile/scrcpy)
+# WhatsApp Andoid Image Scraper
 
-- pip install opencv-python
-- https://pypi.org/project/pytesseract/
-- https://github.com/tesseract-ocr/tesseract
+Basic script that downloads all images of a given WhatsApp chat using Andoid automation.
 
-https://stackoverflow.com/questions/71983792/how-to-paint-a-rectangle-over-words/71984673#71984673
+**Preview**
 
-1. Open Menu
-    Wait 2 sec
-2. Select Share > OCR
-    Wait 2 sec
-3. Select Dropbox > OCR
-    Wait 2 sec
-4. Select "Add" from > OCR
-    Wait 2 sec
-5. Select "Upload here" > 850 2250
-    Wait 2 sec on repeat
-5.1 Check if "File already exists" by looking for Replace > OCR
+![Video](WAIS.gif)
 
-5.2 Select Replace > OCR from above
-    Wait
-6. Wait till uploaded text shows > "Uploaded"
-7. Close Dropbox > 80, 160
-    Wait 1 sec
-8. Swipe to next image
+## Description
 
-Header= 0 100 -> 1080 230
+**Note:** In its current state this script will not work "plug-and-play". It currently 
+(barely) works for this niche use-case, but might be helpful for others get get a starting
+point from.
+
+This scipt uses Python and the **A**ndroid **D**ebug **B**ridge (ADB) to automatically download
+all images within a given chat. It does this by emulating touch inputs (via ADB) and reading
+the screen and locating text (via [Tesseract](https://github.com/tesseract-ocr/tesseract)).
+
+The following steps have been implemented:
+
+0. The newest (first) image of a given chat's gallery should be open.
+1. Select the menu (using xy position)
+1. Select "Share" (using OCR for "Share")
+1. Select Dropbox (using OCR for "Dropbox")
+1. Select "Add to Dropbox" (using OCR for "Add")
+1. Select "Upload here" (using xy position)
+    1. Check if image already exists and reupload if does (using OCR for "Replace")
+1. Close Dropbox (using xy position)
+1. Swipe to next image
+
+## Getting Started
+
+### Dependencies
+
+The following external programs need to exist for this to run:
+* ADB - to control the Andoid phone (I used [this XDA article](https://www.xda-developers.com/install-adb-windows-macos-linux/) to setup)
+* python (>3.6)
+* [Tesseract](https://github.com/tesseract-ocr/tesseract) - for OCR of screenshots
+* [scrcpy](https://github.com/Genymobile/scrcpy) [Optional] - to mirror phone screen to computer
+
+**Note:** I have only tested this on Linux, though no reason it should not work in MacOS/ Windows
+
+### Installing
+
+The install all dependencies run:
+
+```bash
+pip install -r requirements.txt
+```
+
+### Execute the script
+
+* Ensure that your phone has been setup in developer mode and is setup to
+use ADB
+* [Optional] Start `scrcpy`
+* Run the script and following the prompts on screen
+    ```
+    python wais.py
+    ```
+
+
+## Authors
+
+[@OllieFritz](https://olliefritz.com/)
+
+## To-dos
+- [ ] Increase robustness of each step
+- [ ] Refactor to make each step a combination of _Action_, _Collection_ and _Validation_.
+
+- [ ] Make it more generally usable (if interest)
+
+## Acknowledgments
+
+Inspiration, code snippets, etc.
+- [Pytesseract StackOverflow](https://stackoverflow.com/questions/71983792/how-to-paint-a-rectangle-over-words/71984673#71984673)
+- [ADB install](https://www.xda-developers.com/install-adb-windows-macos-linux/)
+- [PYIO Automate's video](https://www.youtube.com/watch?v=6cvGOiurzts)
+- [Engineer Man's video](https://www.youtube.com/watch?v=Du__JfXqsAs)
+
+Thank you to all of the above
--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import interaction as ia
 import locs
 from time import sleep
 
+
+def Stage(func: callable, *args, **kwargs):
+    pass
+
 client = AdbClient(host="127.0.0.1", port=5037)
 print("Select device:")
 for i, device in enumerate(client.devices()):
@@ -26,34 +30,42 @@ while True:
         header = ia.CroppedScreenshot(device, **locs.WHATSAPP_IMAGE_HEADER)
         print(header.contact_text())
         # Action
+        print("  1. Open Image Menu", end=" ")
         ia.tap(device, **locs.WHATSAPP_MENU)
         # Wait
-        sleep(1)
+        sleep(0.5)
         # Collect
         shot = ia.Screenshot(device)
         share_text = shot.search("Share")
         # Validate
         if share_text:
+            print("✔️")
             break
-        input("Fix and continue")
+        input("  1. Fix and continue")
 
+    # Stage("Open image menu", sleep=5).action(ia.CroppedScreenshot).collect().verify()
+
+    # Stage("Open image menu", sleep 5, action=Step(), collect=Step(), verify=Step())
     # 2. Select Share > OCR
     while True:
         # Action
+        print("  2. Select Share", end=" ")
         ia.tap(device, **share_text.xy_location)
         # Wait
-        sleep(1)
+        sleep(1.5)
         # Collect
         shot = ia.Screenshot(device)
         dropbox_text = shot.search("Dropbox")
         # Validate
         if dropbox_text:
+            print("✔️")
             break
-        input("Fix and continue")
+        input("  2. Fix and continue")
     
     # 3. Select Dropbox > OCR
     while True:
         # Action
+        print("  3. Select Dropbox", end=" ")
         ia.tap(device, **dropbox_text.xy_location)
         # Wait
         sleep(1)
@@ -62,39 +74,43 @@ while True:
         add_text = shot.search("Add")
         # Validate
         if add_text:
+            print("✔️")
             break
-        input("Fix and continue")
+        input("  3. Fix and continue")
 
     # 4. Select "Add" from > OCR
     while True:
         # Action
+        print("  4. Select 'Add'", end=" ")
         ia.tap(device, **add_text.xy_location)
         # Wait
-        sleep(2)
+        sleep(1.5)
         # Collect
         shot = ia.Screenshot(device)
         upload_text = shot.search("Upload")
         # Validate
         if upload_text:
+            print("✔️")
             break
-        input("Fix and Continue")
+        input("  4. Fix and Continue")
 
     # 5. Select "Upload here" > 850 2250
     while True:
         # Action
+        print("  5. Select 'Upload here'", end=" ")
         ia.tap(device, **locs.DROPBOX_UPLOAD_BUTTON)
         # Wait
-        sleep(3)
+        sleep(5)
         # Collect
         shot = ia.Screenshot(device)
         replace_text = shot.search("Replace")
         if replace_text:
-            print("Already Exists, replace")
+            print("  replacing ", end="")
             while True:
                 #Action
                 ia.tap(device, **replace_text.xy_location)
                 # Wait
-                sleep(3)
+                sleep(4)
                 # Collect
                 shot = ia.Screenshot(device)
                 replace_text = shot.search("Replace")
@@ -103,21 +119,25 @@ while True:
                 input("Fix and Continue")
         uploaded_text = shot.search("Uploaded")
         if uploaded_text:
+            print("✔️")
             break
         input("Fix and Continue")
             
-    # 7. Close Dropbox
+    # 6. Close Dropbox
     while True:
         # Action
+        print("  6. Close Dropbox", end=" ")
         ia.tap(device, **locs.DROPBOX_CLOSE_BUTTON)
         # Wait 
-        sleep(1)
+        sleep(0.5)
         shot = ia.Screenshot(device)
         uploaded_text = shot.search("Uploaded")
         if not uploaded_text:
+            print("✔️")
             break
-        input("Fix and Continue")
+        input("  6. Fix and Continue")
 
-    # 8. Swuipe to next image
+    # 7. Swuipe to next image
+    print("  7. Next Image")
     ia.swipe_left(device)
-    sleep(1)
+    sleep(0.5)
